f49da6c9514ae82c89e97ad96bc25f963449768d
K4ILG <k4ilg@arrl.net>
2020-05-03 22:48:00 -0400

    Use AltSoftSerial in place of SoftwareSerial.
    Software serial was causing missed messges in TinyGPS++.
    Had to switch GPS serial pins to TX->9, RX->8.

--- satellogger.ino
+++ satellogger.ino
@@ -11,7 +11,7 @@
  * 
  */
 #define BANNER "Satellogger :: K4ILG"
-#define VERSION "0.8.1"
+#define VERSION "0.8.2"
 #define COPYRIGHT "(c) 2020 - Donald Purnhagen"
 #define INVALID "Searching...        "
 
@@ -19,18 +19,18 @@
 #include <hd44780.h>                       // hd44780
 #include <hd44780ioClass/hd44780_I2Cexp.h> // i2c expander
 #include <TinyGPS++.h>
+#include <AltSoftSerial.h>                // serial port for gps
 
 // For serial monitor debugging.
 // Comment out the define to disable debugging.
 #define DEBUG_PORT Serial
 
-// GPS stuff.
-const long COORD_SCALING_FACTOR = 100000L;
-#include <SoftwareSerial.h>                // serial port for gps
-const int GPS_RX = 3; // TX of SoftwareSerial.
-const int GPS_TX = 2; // RX of SoftwareSerial.
-SoftwareSerial gpsPort(GPS_TX, GPS_RX);
+// Status on the bottom line of the 4 row display.
+// Comment out the define to disable status line.
+#define STATUS_LINE
 
+// GPS serial port.
+AltSoftSerial gpsPort; // GPS TX to pin 8, GPS RX to pin 9
 
 // LCD display address and geometry.
 const int LCD_ADDRESS = 0x27;
@@ -46,19 +46,8 @@ byte logo[] = {
 };
 
 // Global variables.
-static TinyGPSPlus gps;
+TinyGPSPlus gps;
 hd44780_I2Cexp lcd(LCD_ADDRESS); // Declare lcd object at fixed i2c address.
-
-// Lat and lon are stored as longs. This prints them as decimal degrees.
-static void longToDecimal(long lValue, char *szBuff) {
-    long lWhole = lValue / COORD_SCALING_FACTOR;
-    long lRemove = lWhole * COORD_SCALING_FACTOR;
-    long lFraction = lValue - lRemove;
-    if (0 > lFraction) {
-        lFraction *= -1L;
-    }
-    sprintf(szBuff, "%ld.%05ld", lWhole, lFraction);
-}
 
 void setup() {
     int status;
@@ -84,7 +73,7 @@ void setup() {
     lcd.write(0);
     lcd.write(1);
 
-#if defined (DEBUG_PORT)
+    #if defined (DEBUG_PORT)
     {
         DEBUG_PORT.begin(9600);
         DEBUG_PORT.flush();
@@ -94,18 +83,23 @@ void setup() {
         DEBUG_PORT.print(F("TinyGPS++ version "));
         DEBUG_PORT.println(TinyGPSPlus::libraryVersion());
     }
-#endif // DEBUG_PORT
-    
+    #endif // DEBUG_PORT
+
     gpsPort.begin(9600);
     delay(1000);
 }
 
 void loop() {
-    static uint8_t iPrevSecond = 255;
+    static int iSatCount = 0xffff;
+    static int iPrevSecond = 0xffff;
     while (gpsPort.available()) {
         int c = gpsPort.read();
-        if (gps.encode(c)) {
+        bool bFix = gps.encode(c);
+        if (bFix) {
             // Fix is ready.
+            if (0 < gps.satellites.value()) {
+                iSatCount = gps.satellites.value();
+            }
             lcd.setCursor(0, 2);
             if (!gps.time.isValid() || !gps.date.isValid()) {
                 lcd.print(INVALID);
@@ -117,6 +111,22 @@ void loop() {
                             gps.date.year(), gps.date.month(), gps.date.day(), 
                             gps.time.hour(), gps.time.minute(), iSecond);
                     lcd.print(szTimestamp);
+                    
+                    #if defined (STATUS_LINE)
+                    {
+                        size_t iPos = 0;
+                        lcd.setCursor(0, 3);
+                        iPos += lcd.print(gps.location.lat(), 3);
+                        iPos += lcd.print(" ");
+                        iPos += lcd.print(gps.location.lng(), 3);
+                        iPos += lcd.print(" ");
+                        iPos += lcd.print(iSatCount);
+                        while (LCD_COLS > iPos) {
+                            iPos += lcd.print(" ");
+                        }
+                    }
+                    #endif // STATUS_LINE
+                    
                     iPrevSecond = iSecond;
                     #if defined (DEBUG_PORT)
                     {
@@ -124,8 +134,7 @@ void loop() {
                         DEBUG_PORT.print(",");
                         DEBUG_PORT.print(gps.location.lng(), 5);
                         DEBUG_PORT.print(",");
-                        // Number of satellites is not working in TinyGPSPlus.
-                        DEBUG_PORT.print(gps.satellites.value());
+                        DEBUG_PORT.print(iSatCount);
                         DEBUG_PORT.print(",");
                         DEBUG_PORT.println(szTimestamp);
                     }
